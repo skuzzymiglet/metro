fcb173cbb189fc93b25378fb658eba51fa5c117a
skuzzymiglet <skuzzymiglet@gmail.com>
2020-07-15 12:47:05 +0100

    Still broken

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 
 	"github.com/eiannone/keyboard"
 	"github.com/faiface/beep"
@@ -39,85 +41,73 @@ func toTempo(s beep.StreamSeeker, f beep.Format, tempo int) *beep.Resampler {
 	// return beep.ResampleRatio(2, , s)
 }
 
+func getReadCloser(name string) (io.ReadCloser, error) {
+	var f io.ReadCloser
+	var err error
+	if _, err := pkger.Stat(name); err == nil {
+		fmt.Println("embedded")
+		f, err = pkger.Open(name)
+	} else {
+		fmt.Println("os.file")
+		f, err = os.Open(name)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	return f, err
+}
+
+func getStreamer(r io.ReadCloser) (beep.StreamSeekCloser, beep.Format, error) {
+	var buf bytes.Buffer
+	tee := io.TeeReader(r, &buf)
+	mime, err := mimetype.DetectReader(tee)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var streamer beep.StreamSeekCloser
+	var format beep.Format
+	if mime.Is("audio/flac") {
+		fmt.Println("flac")
+		streamer, format, err = flac.Decode(ioutil.NopCloser(&buf))
+	} else if mime.Is("audio/mp3") {
+		fmt.Println("mp3")
+		streamer, format, err = mp3.Decode(ioutil.NopCloser(&buf))
+	} else if mime.Is("audio/wav") {
+		streamer, format, err = wav.Decode(ioutil.NopCloser(&buf))
+	} else if mime.Is("audio/ogg") {
+		streamer, format, err = vorbis.Decode(ioutil.NopCloser(&buf))
+	} else {
+		log.Fatal("Sample is not flac, mp3, wav or ogg")
+	}
+	return streamer, format, err
+}
+
 func main() {
 	pkger.Include("/samples/tabla_te_m.flac")
 
-	fname := flag.String("f", "samples/tabla_dhec.flac", "file")
+	fname := flag.String("f", "/samples/tabla_dhec.flac", "file")
 	tempo := flag.Int("t", 120, "tempo")
 	beats := flag.Int("b", 4, "beats")
 	onSymbol := flag.String("o", "🔴", "Symbol for current beat")
 	offSymbol := flag.String("O", "⭕", "Symbol for all other beats")
 	flag.Parse()
-	var f io.ReadCloser
-	if _, err := pkger.Stat(*fname); err == nil {
-		fmt.Println("embedded")
-		f, err = pkger.Open(*fname)
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		fmt.Println("os.file")
-		f, err = os.Open(*fname)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	var streamer beep.StreamSeekCloser
-	var format beep.Format
-	b := make([]byte, 64)
-	_, err := f.Read(b)
-	// fmt.Println(f.Stat().Size())
-	fmt.Printf("%T %#v\n", f, f)
-	mime, err := mimetype.DetectReader(f)
-	defer f.Close()
+
+	reader, err := getReadCloser(*fname)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(mime.String())
-	fmt.Println(f)
-	if mime.Is("audio/flac") {
-		fmt.Println("flac")
-		streamer, format, err = flac.Decode(f)
-	} else if mime.Is("audio/mp3") {
-		fmt.Println("mp3")
-		streamer, format, err = mp3.Decode(f)
-	} else if mime.Is("audio/wav") {
-		streamer, format, err = wav.Decode(f)
-	} else if mime.Is("audio/ogg") {
-		streamer, format, err = vorbis.Decode(f)
-	} else {
-		log.Fatal("Sample is not flac, mp3, wav or ogg")
-	}
-	fmt.Println(io.LimitReader(f, 100))
+	streamer, format, err := getStreamer(reader)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	// streamer, format, err := mp3.Decode(f)
-	// if err != nil {
-	// 	fmt.Println("not mp3")
-	// 	streamer, format, err = flac.Decode(f)
-	// 	log.Println(err)
-	// 	if err != nil {
-	// 		fmt.Println("not flac")
-	// 		streamer, format, err = wav.Decode(f)
-	// 		log.Println(err)
-	// 		if err != nil {
-	// 			fmt.Println("not wav")
-	// 			streamer, format, err = vorbis.Decode(f)
-	// 			log.Println(err)
-	// 			if err != nil {
-	// 				fmt.Println("not vorbis")
-	// 				log.Println("Sample is none of supported formats: mp3, flac, wav, vorbis")
-	// 				log.Fatal(err)
-	// 			}
-	// 		}
-	// 	}
-	// }
+	fmt.Println(streamer.Len())
 
-	defer streamer.Close()
 	buffer := beep.NewBuffer(format)
+	fmt.Println(buffer.Len())
 	toAppend := toTempo(streamer, format, *tempo)
 	buffer.Append(toAppend)
+	streamer.Close()
+	fmt.Println(buffer.Len())
 	loop := beep.Loop(-1, buffer.Streamer(0, buffer.Len()))
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 	keysEvents, err := keyboard.GetKeys(10)
@@ -136,7 +126,11 @@ func main() {
 			}
 		}
 	}()
-	go speaker.Play(beep.Seq(loop))
+	go func() {
+		fmt.Println("ree")
+		speaker.Play(beep.Seq(loop))
+		fmt.Println("ree")
+	}()
 	for {
 		c := time.Tick(time.Duration(60000 / *tempo) * time.Millisecond)
 		currentBeat := 0
